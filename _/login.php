8a57274257928d5aec64c2df6f5591d7dc97a6e3
Athiththan <code19111995@gmail.com>
2018-08-29 14:35:02 +0530

    add description

--- _/login.php
+++ _/login.php
@@ -19,30 +19,66 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login - CSRF STP</title>
 
-    <?php include (realpath(__DIR__)."/addons/header.php") ?>    
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Login - CSRF STP</title>
+
+	<?php include (realpath(__DIR__)."/addons/header.php") ?>
 
 </head>
+
 <body>
-    
-    <div class="container">
-        <form action="login.php" method="POST">
-            <div>
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username" />
-            </div>
-            <div>
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" />
-            </div>
-            <button type="submit" value="login" name="login">Login</button>
-        </form>
-    </div>
+
+	<div class="container">
+		<div class="row">
+
+			<!-- Sign in block -->
+			<div class="col-md-4 mx-auto order-12">
+				<div class="card my-5 p-3 shadow">
+					<div class="card-body">
+						<h5 class="card-title text-center">Sign In</h5>
+
+						<!-- Sign in Form -->
+						<form class="mt-5 mb-3" action="login.php" method="POST">
+							<div class="form-group">
+								<label for="username">Username</label>
+								<input type="text" class="form-control" id="username" name="username" required autofocus/>
+							</div>
+							<div class="form-group">
+								<label for="password">Password</label>
+								<input type="password" class="form-control" id="password" name="password" required/>
+							</div>
+							<button type="submit" class="btn btn-primary btn-block mt-5" name="login">Login</button>
+						</form>
+						<!-- End Sign in Form -->
+
+					</div>
+				</div>
+			</div>
+			<!-- End Sign in block -->
+
+			<!-- Description block -->
+			<div class="col-md-6 mx-auto my-5 order-1">
+				<h4>CSRF Synchronizer Token Pattern</h4>
+				<hr class="my-4">
+				<p>
+					This is a sample PHP application implemented to explain the <b>Cross Site Request Forgery (CSRF) - <i>Synchronizer Token
+							Pattern
+						</i></b>.<br/><br/>You can use the following credentials to login to the system.
+				</p>
+				<ul>
+					<li><span>Username: csrf</span></li>
+					<li><span>Password: token</span></li>
+				</ul>
+			</div>
+			<!-- End Description block -->
+
+		</div>
+	</div>
 
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,46 +20,83 @@
     <?php include (realpath(__DIR__)."/_/addons/header.php") ?>
 
 </head>
+
 <body>
-    
-    <?php
-        if(isset($_COOKIE['csrf_session_cookie'])){
-            echo 
-                '<form method="POST" action="/src/service.php">
-                    <input type="submit" value="Logout" name="logout"/>
-                </form>';
-        }
-    ?>
 
-    Hi
+    <ul class="nav justify-content-center mt-3">
+        <?php
+            if(isset($_COOKIE['csrf_session_cookie'])){
+                echo 
+                    '<li class="nav-item">
+                        <form class="nav-link" method="POST" action="/src/service.php">
+                            <button class="btn btn-link" type="submit" value="Logout" name="logout">Lgout</button>
+                        </form>
+                    </li>';
+            }
+        ?> 
+    </ul>
 
-    <form action="/src/service.php" method="POST">
-        <input type="hidden" id="csrf_token" name="csrf_token" value="csrf" />
-        <div>
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name"/>
+    <div class="container">
+        <div class="row">
+
+            <!-- csrf form block -->
+            <div class="col-md-4 mx-auto">
+                <div class="card shadow my-5 p-3">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">CSRF Token Example Form</h5>
+                        <hr class="my-4">
+
+                        <!-- csrf form -->
+                        <form class="mt-3 mb-3" action="/src/service.php" method="POST">
+
+                            <!-- csrf hidden input field -->
+                            <input type="hidden" id="csrf_token" name="csrf_token" value="csrf" />
+
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" class="form-control" id="name" name="name" />
+                            </div>
+                            <div class="form-group">
+                                <label for="some">Some</label>
+                                <input type="text" class="form-control" id="some" name="some" />
+                            </div>
+                            <button type="submit" class="btn btn-success btn-block mt-5" name="submit">Submit</button>
+                        </form>
+                        <!-- End csrf form -->
+
+                    </div>
+                </div>
+            </div>
+            <!-- End csrf form block -->
+
+            <!-- Description block -->
+            <div class="col-md-6 mx-auto my-5">
+                <h4>CSRF Synchronizer Token Pattern</h4>
+                <hr class="my-4">
+                <p>
+                    Lorem Ipsum
+                </p>
+            </div>
+            <!-- End Description block -->
+
         </div>
-        <div>
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name"/>
-        </div>
-        <button type="submit" name="submit" value="submit">Submit</button>
-    </form>
+    </div>
 
+    <!-- CSRF Token retrieve | ajax call to the service.php -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             $.ajax({
                 url: '/src/service.php',
                 type: 'post',
                 data: {
                     'csrf_request': '<?php echo $_COOKIE['csrf_session_cookie'] ?>'
                 },
-                success: function(data) {
+                success: function (data) {
                     console.log(data);
                     document.getElementById("csrf_token").value = data;
                 },
-                error: function(xhr, ajaxOptions, thrownError) {
+                error: function (xhr, ajaxOptions, thrownError) {
                     console.log("Something went wrong :: " + xhr.responseText);
                 }
             });
@@ -66,4 +104,5 @@
     </script>
 
 </body>
+
 </html>
