11734e1f118af1fe077d681464efead7ad741a2d
Athiththan <code19111995@gmail.com>
2018-08-29 11:55:19 +0530

    implement csrf

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     session_start();
 
     if(!isset($_SESSION['csrf_session'])){
-        header("location: ./views/login.php");
+        header("location: ./_/login.php");
     }
 
 ?>
@@ -16,12 +16,54 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CSRF Synchronizer Token Pattern</title>
 
-    <?php include (realpath(__DIR__)."/views/addons/header.php") ?>
+    <?php include (realpath(__DIR__)."/_/addons/header.php") ?>
 
 </head>
 <body>
     
+    <?php
+        if(isset($_COOKIE['csrf_session_cookie'])){
+            echo 
+                '<form method="POST" action="/src/service.php">
+                    <input type="submit" value="Logout" name="logout"/>
+                </form>';
+        }
+    ?>
+
     Hi
+
+    <form action="/src/service.php" method="POST">
+        <input type="hidden" id="csrf_token" name="csrf_token" value="csrf" />
+        <div>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name"/>
+        </div>
+        <div>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name"/>
+        </div>
+        <button type="submit" name="submit" value="submit">Submit</button>
+    </form>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $.ajax({
+                url: '/src/service.php',
+                type: 'post',
+                data: {
+                    'csrf_request': '<?php echo $_COOKIE['csrf_session_cookie'] ?>'
+                },
+                success: function(data) {
+                    console.log(data);
+                    document.getElementById("csrf_token").value = data;
+                },
+                error: function(xhr, ajaxOptions, thrownError) {
+                    console.log("Something went wrong :: " + xhr.responseText);
+                }
+            });
+        });
+    </script>
 
 </body>
 </html>
--- src/service.php
+++ src/service.php
@@ -1,3 +1,15 @@
 <?php
 
+    function logout() {
+        unset($_COOKIE['csrf_session_cookie']);
+        setcookie('csrf_session_cookie', null, -1, '/');
+        unset($_SESSION);
+        header("location: ./../../_/login.php");
+    }
+
+    if(isset($_POST['logout'])){
+        logout();
+    } else if (isset($_POST['csrf_request'])){
+        echo "Somethings here";
+    }
 ?>
